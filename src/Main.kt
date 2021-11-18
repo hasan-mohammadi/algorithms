306a9736f8e069ba39fad512ad812a8f8456f938
hasan.mohammadi <h4347255@gmail.com>
2021-11-18 18:26:59 +0330

    +

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,13 @@
 fun main() {
+    val array = arrayListOf(4, 7, 2, 1, 3, 5)
+
     /** inversionsCount() is a extension function that can be called for any int array */
-    print("Inversion Count  is : ${arrayListOf(4, 7, 2, 1, 3, 5).inversionsCount()} \n")
+    print("Inversion Count  is : ${array.inversionsCount()} \n")
 
 
     /** inversionsCount() take a lambda function as the parameter that can be used for change inversion counting conditions */
     print(
-        "Inversion Count with specific conditions is : ${arrayListOf(4, 7, 2, 1, 3, 5).inversionsCount(
+        "Inversion Count with specific conditions is : ${array.inversionsCount(
             comparator = { l, r ->
                 return@inversionsCount (l > r && ((l % 2 != 0) || (l % 2 == 0 && r % 2 == 0)))
             })
